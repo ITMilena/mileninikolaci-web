5b55f8632e713c4cdd32402fa87b4a3a86b120da
ITMilena <milena.zivkovic2010000@gmail.com>
2026-01-18 17:06:32 +0100

    Update urls.py

--- kolaci/urls.py
+++ kolaci/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path
-from kolaci import views
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.pocetna, name='pocetna'),
+    path('', views.home, name='home'),
     path('ponuda/', views.ponuda, name='ponuda'),
     path('kontakt/', views.kontakt, name='kontakt'),
+    path('o-nama/', views.onama, name='onama'),
 ]
+
+
