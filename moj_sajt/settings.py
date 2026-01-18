46af2a98dbb7289df3bb599bb8213304af7f341e
ITMilena <milena.zivkovic2010000@gmail.com>
2026-01-18 16:10:27 +0100

    Update settings.py

--- moj_sajt/settings.py
+++ moj_sajt/settings.py
@@ -116,11 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Gde Django da traži tvoje statičke fajlove tokom razvoja
 STATICFILES_DIRS = [
-    BASE_DIR / 'kolaci' / 'static',
+    BASE_DIR / 'kolaci/static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
