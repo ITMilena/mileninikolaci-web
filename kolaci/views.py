c0237cf2a50a8bdac0e5462f6ae76522afbf2cb7
ITMilena <milena.zivkovic2010000@gmail.com>
2026-01-18 15:22:25 +0100

    Add files via upload

---
+++ kolaci/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'kolaci/home.html')
+
+
+def ponuda(request):
+    return render(request, 'kolaci/ponuda.html')
+
+
+def kontakt(request):
+    return render(request, 'kolaci/kontakt.html')
+
+
+def onama(request):
+    return render(request, 'kolaci/onama.html')
