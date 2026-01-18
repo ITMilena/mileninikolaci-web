3b106d8600ae69c27581b8abbc07a0d1f8251364
ITMilena <milena.zivkovic2010000@gmail.com>
2026-01-18 16:36:31 +0100

    Update views.py

--- kolaci/views.py
+++ kolaci/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
 
-
-def home(request):
-    return render(request, 'kolaci/home.html')
-
+def pocetna(request):
+    return render(request, 'kolaci/pocetna.html')
 
 def ponuda(request):
     return render(request, 'kolaci/ponuda.html')
 
-
 def kontakt(request):
     return render(request, 'kolaci/kontakt.html')
 
-
-def onama(request):
-    return render(request, 'kolaci/onama.html')
